4c1ea8d2b650e7163e47e6d66e7d3742dc1f651b
shaeeb <shaeebali@gmail.com>
2023-11-20 10:38:06 +0000

    implement random/get all routes

--- server.js
+++ server.js
@@ -11,14 +11,20 @@ app.use(express.static('public'));
 
 //Get random quote via the /api/quotes/random route
 app.get('/api/quotes/random', (req, res, next) => {
-
+  const quote = getRandomElement(quotes);
+  res.send( {quote: quote} );
 });
 
 //Get all quotes via /api/quotes route
 app.get('/api/quotes', (req, res, next) => {
-  const allQuotes = req.query;
-  res.send(allQuotes);
-  // console.log(quotes);
+  if(!req.query.hasOwnProperty('person')) {
+    res.send({quotes: quotes});
+  } else {
+    const filterQuote = quotes.filter( (element) => {
+      element.person === req.query.person
+    });
+    res.send({quote: filterQuote});
+  }
 });
 
 //Listening on desired port and log to console for monitoring
